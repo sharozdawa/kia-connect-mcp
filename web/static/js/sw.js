21d9a45374441056fdb91264918bf2c19c773c8d
Sharoz Dawa <sharozdawa@gmail.com>
2026-05-01 20:33:06 +0530

    Remove dead reference to non-existent app.css from service worker
    The ASSETS const wasn't actually used (install handler only calls
    skipWaiting), but the reference was misleading. Caught by fresh-clone
    verification.

--- web/static/js/sw.js
+++ web/static/js/sw.js
@@ -1,5 +1,4 @@
 const CACHE = 'kia-connect-v1';
-const ASSETS = ['/', '/static/css/app.css'];
 
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
